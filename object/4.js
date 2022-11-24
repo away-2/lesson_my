e40069319501537b66f3442098dbc1e8e2f617f9
away-2 <116934224+away-2@users.noreply.github.com>
2022-11-24 21:18:18 +0800

    JS之阮一峰

---
+++ object/4.js
@@ -0,0 +1,11 @@
+function Cat(name,color) {
+//{} Object
+    return {
+             name:name,
+             color:color
+             }
+       }
+    //基于对象的
+    var cat1=Cat("大毛","黄色");
+    var cat3=Cat('三毛','黑色');
+
