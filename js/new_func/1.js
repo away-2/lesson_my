9f0f8035707f7e0e8ab2908dc4ae4cf822df2ae4
away-2 <1968642178@qq.com>
2022-12-14 23:56:46 +0800

    this

---
+++ js/new_func/1.js
@@ -0,0 +1,24 @@
+var stuA = {
+    name:'踩踩呀'
+};
+
+var stuB = {
+    name:'六六呀'
+};
+
+
+function sayHi(...args){
+    console.log(arguments,...args);
+    console.log(`你好，我是${this.name}`)
+}
+Object.prototype.sayHi =sayHi;
+// stuA,stuB 都能  sayHi
+// call  改天换命，  手动指定this 的指向
+// this -> 调用对象stuA stuB
+// sayHi.call(stuB);
+// sayHi.call(stuA);
+// stuA.sayHi();
+sayHi.apply(stuB,[1,2]);
+sayHi.call(stuB,1,2);
+// var func = sayHi.bind(stuB);
+// func();
