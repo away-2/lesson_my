141491d3f7848a577b316f0112c98ec68e7a2d45
away-2 <116934224+away-2@users.noreply.github.com>
2023-01-12 23:39:01 +0800

    仿瑞幸首页

---
+++ index/childCpns/rx__bd/rx__bd.js
@@ -0,0 +1,37 @@
+// pages/index/childCpns/rx__bd/rx__bd.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        swipers:[
+            {
+                  "id": 1,
+                  "pic": "https://pic2.zhimg.com/v2-4fbee414f88e1889a39e6e451baaefe2_r.jpg?source=1940ef5c"
+                },
+                {
+                      "id":2,
+                      "pic":"https://tse3-mm.cn.bing.net/th/id/OIP-C.IcLFCHTmgeJq055hfCFQJwHaE8?w=282&h=188&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+                },
+                {
+                      "id":3,
+                      "pic":"https://ts1.cn.mm.bing.net/th/id/R-C.3aa89a8b1e6bcea3bdce69bb51c5b1de?rik=gowEgdG2WqPiQg&riu=http%3a%2f%2fresources.jsmo.xin%2ftemplates%2fupload%2f20295%2f202008%2f1596273131622.png&ehk=frq8hY%2bRNOoRewsfhZ6WiiqDFMly4%2bWXrO1TlvbUYlw%3d&risl=&pid=ImgRaw&r=0"
+                }
+                
+          ],
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
---
+++ index/childCpns/rx__top/rx__top.js
@@ -0,0 +1,49 @@
+// pages/index/childCpns/rx__top/rx__top.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        slides:[
+            {
+                  "id": 1,
+                  "image": "https://pic2.zhimg.com/v2-4fbee414f88e1889a39e6e451baaefe2_r.jpg?source=1940ef5c"
+                },
+                {
+                      "id":2,
+                      "image":"https://tse3-mm.cn.bing.net/th/id/OIP-C.IcLFCHTmgeJq055hfCFQJwHaE8?w=282&h=188&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+                },
+                {
+                      "id":3,
+                      "image":"https://tse2-mm.cn.bing.net/th/id/OIP-C.FKtLjjZoki6ISHComjxH9AHaE8?w=280&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+                },
+                {
+                      "id":4,
+                      "image":"https://tse3-mm.cn.bing.net/th/id/OIP-C.DT-0ddS7ludliK1ypy1pqQHaEN?w=311&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+                },
+                {
+                      "id":5,
+                      "image":"https://tse4-mm.cn.bing.net/th/id/OIP-C.KEYCzZeIyTIG_I-c9n_UyAHaEK?w=302&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+                },
+                {
+                      "id":6,
+                      "image":"https://tse4-mm.cn.bing.net/th/id/OIP-C.s_ugoSG9gpX7gGfT-h_YJAHaE7?w=244&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+                }
+                
+          ]
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
